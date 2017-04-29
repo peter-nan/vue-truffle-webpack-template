d8667ac7014f97b565df682b81b2df0d39fa0793
wilfreddenton <dentonw3@gmail.com>
2017-04-29 18:23:34 -0400

    now includes the route change mutation

--- template/src/store/mutation-types.js
+++ template/src/store/mutation-types.js
@@ -1,3 +1,4 @@
+export const ROUTE_CHANGED = 'router/ROUTE_CHANGED'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 export const UPDATE_ACCOUNT = 'UPDATE_ACCOUNT'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 export const UPDATE_ADDRESS = 'UPDATE_ADDRESS'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 export const UPDATE_AMOUNT = 'UPDATE_AMOUNT'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
